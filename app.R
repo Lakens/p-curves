e24781361593cd6f234032c6a27cae51488af0c3
Daniel Lakens <Lakens@gmail.com>
2017-04-19 13:59:42 +0200

    added text

--- app.R
+++ app.R
@@ -1,3 +1,7 @@
+#To do: Add power curvr
+#       Scale y-axis p-curve
+#       Include Likelihood H1/H0
+
 library(shiny)
 library(shinythemes)
 ui <- fluidPage(theme=shinytheme("flatly"),
@@ -5,7 +9,7 @@ ui <- fluidPage(theme=shinytheme("flatly"),
   sidebarLayout(
     sidebarPanel(numericInput("N", "Subject per group:", 50, min = 1, max = 1000),
                  sliderInput("d", "Cohen's d effect size:", min = 0, max = 3, value = 0.5, step= 0.01),
-                 sliderInput("p_upper", "P-value (upper limit):", min = 0, max = 1, value = 0.05, step= 0.01),
+                 sliderInput("p_upper", "alpha, or P-value (upper limit):", min = 0, max = 1, value = 0.05, step= 0.001),
                  uiOutput("p_low")
     ),
     mainPanel(
@@ -36,7 +40,7 @@ server <- function(input, output) {
     #p-value function
     pdf2_t <- function(p) 0.5 * dt(qt(p/2,2*N-2,0),2*N-2,ncp)/dt(qt(p/2,2*N-2,0),2*N-2,0) + dt(qt(1-p/2,2*N-2,0),2*N-2,ncp)/dt(qt(1-p/2,2*N-2,0),2*N-2,0)
     par(bg = "aliceblue")
-    plot(-10,xlab="P-value", ylab="density of p-values", axes=FALSE,
+    plot(-10,xlab="P-value", ylab="Density", axes=FALSE,
          main=paste("P-curve"), xlim=c(0,1),  ylim=c(0, ymax))
     abline(v = seq(0,1,0.1), h = seq(0,ymax,5), col = "lightgray", lty = 1)
     axis(side=1, at=seq(0,1, 0.1), labels=seq(0,1,0.1))
@@ -45,9 +49,7 @@ server <- function(input, output) {
     cord.y <- c(0,pdf2_t(seq(p_lower, p_upper, 0.001)),0)
     polygon(cord.x,cord.y,col=rgb(1, 0, 0,0.5))
     curve(pdf2_t, 0, 1, n=1000, col="black", lwd=2, add=TRUE)
-    p_u<-1 + pt(qt(p_upper/2,2*N-2,0),2*N-2,ncp) - pt(qt(1-p_upper/2,2*N-2,0),2*N-2,ncp) #two-tailed
-    p_l<-1 + pt(qt(p_lower/2,2*N-2,0),2*N-2,ncp) - pt(qt(1-p_lower/2,2*N-2,0),2*N-2,ncp) #two-tailed
-  })
+})
   output$cdf <- renderPlot({
     N<-input$N
     d<-input$d
@@ -62,7 +64,7 @@ server <- function(input, output) {
     cdf2_t<-function(p) 1 + pt(qt(p/2,2*N-2,0),2*N-2,ncp) - pt(qt(1-p/2,2*N-2,0),2*N-2,ncp)
   
     par(bg = "aliceblue")
-    plot(-10,xlab="P-value", ylab="density of p-values", axes=FALSE,
+    plot(-10,xlab="P-value", ylab="Probability", axes=FALSE,
          main=paste("Cumulative P-curve"), xlim=c(0,1),  ylim=c(0, 1))
     abline(v = seq(0,1,0.1), h = seq(0,1,0.1), col = "lightgray", lty = 1)
     axis(side=1, at=seq(0,1, 0.1), labels=seq(0,1,0.1))
@@ -74,13 +76,17 @@ server <- function(input, output) {
   })
   # make dynamic slider 
   output$p_low <- renderUI({
-    sliderInput("p_lower", "P-value (lower limit):", min = 0, max = input$p_upper, value = 0, step= 0.01)
+    sliderInput("p_lower", "P-value (lower limit):", min = 0, max = input$p_upper, value = 0, step= 0.001)
   })
   output$pow <- renderText({
     N<-input$N
     d<-input$d
+    p_upper<-input$p_upper
+    p_lower<-input$p_lower
     ncp<-(input$d*sqrt(N/2)) #Calculate non-centrality parameter d
-    paste("Statistical Power:",round((1 + pt(qt(input$p_upper/2,2*N-2,0),2*N-2,ncp) - pt(qt(1-input$p_upper/2,2*N-2,0),2*N-2,ncp)),digits=2),".")
+    p_u<-1 + pt(qt(p_upper/2,2*N-2,0),2*N-2,ncp) - pt(qt(1-p_upper/2,2*N-2,0),2*N-2,ncp) #two-tailed
+    p_l<-1 + pt(qt(p_lower/2,2*N-2,0),2*N-2,ncp) - pt(qt(1-p_lower/2,2*N-2,0),2*N-2,ncp) #two-tailed
+    paste("Below, you can see the p-value distribution for a two-sided independent t-test with",N,"participants in each group, and a true effect size of d =",d,". The statistical power (using and alpha of",p_upper,", based on the upper p-value limit) is",100*round((1 + pt(qt(input$p_upper/2,2*N-2,0),2*N-2,ncp) - pt(qt(1-input$p_upper/2,2*N-2,0),2*N-2,ncp)),digits=4),"%. In the long run, you can expect ",100*round(p_u-p_l, 4),"% of p-values to fall in the selected area between p = ",p_lower,"and p = ",p_upper,".")
   })
 }
 shinyApp(ui = ui, server = server)
